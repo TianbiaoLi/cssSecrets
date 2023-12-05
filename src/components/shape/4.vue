56a9805105b7955366b57cddc87c7aed67b9df95
TianbiaoLi <995953089@qq.com>
2023-12-05 13:28:50 +0800

    形状卡片++

--- src/components/shape/4.vue
+++ src/components/shape/4.vue
@@ -11,20 +11,37 @@
   </el-tooltip>
   <pre id="htmlDetail" v-html="htmlDetail"></pre>
   <pre id="cssDetail">{{ cssDetail }}</pre>
-  <pre id="sassDetail">{{ cssDetail }}</pre>
+  <pre id="sassDetail">{{ sassDetail }}</pre>
 </template>
 <script>
 export default {
   setup() {
     const htmlDetail = '<div id="main"></div>';
-    const cssDetail = `transform: rotate(45deg);
-overflow: hidden;
-border: 1px dotted;
-img{
-  max-width: 100%;
-  transform: rotate(-45deg) scale(1.42);
+    const cssDetail = `#main {
+  border: 1px dotted;
+}
+#main > img {
+  width: 100%;
+  height: 100%;
+  clip-path: polygon(50% 0, 100% 50%, 50% 100%, 0 50%);
+  transition: 1s clip-path;
+}
+#main > img:hover {
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
 }`;
-    return { htmlDetail, cssDetail };
+    const sassDetail = `#main {
+  border: 1px dotted;
+  img {
+    width: 100%;
+    height: 100%;
+    clip-path: polygon(50% 0, 100% 50%, 50% 100%, 0 50%);
+    transition: 1s clip-path;
+    &:hover {
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+    }
+  }
+}`;
+    return { htmlDetail, cssDetail,sassDetail };
   },
 };
 </script>
--- src/components/shape/index.vue
+++ src/components/shape/index.vue
@@ -4,6 +4,10 @@
     <card title="平行四边形"><two /></card>
     <card title="菱形 基于变形"><three /></card>
     <card title="菱形 基于裁切路径"><four /></card>
+    <card title="切角效果"><five /></card>
+    <card title="切角效果 弧形"><six /></card>
+    <card title="切角效果 裁切路径"><seven /></card>
+    <card title="梯形效果"><eight /></card>
   </div>
 </template>
 <script>
@@ -12,6 +16,10 @@ import one from "./1.vue";
 import two from "./2.vue";
 import three from "./3.vue";
 import four from "./4.vue";
+import five from "./5.vue";
+import six from "./6.vue";
+import seven from "./7.vue";
+import eight from "./8.vue";
 
 export default {
   components: {
@@ -20,6 +28,10 @@ export default {
     two,
     three,
     four,
+    five,
+    six,
+    seven,
+    eight
   },
 };
 </script>
